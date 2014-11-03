92f10898655a091bfdb2f9e961a9fd6afe85f1a4
Daniel Nguyen <dxungngh@gmail.com>
2014-11-03 21:21:13 +0700

    main activity

--- ReadingBook/app/src/main/java/com/daniel/readingbook/book1/activity/MainActivity.java
+++ ReadingBook/app/src/main/java/com/daniel/readingbook/book1/activity/MainActivity.java
@@ -1,40 +1,16 @@
 package com.daniel.readingbook.book1.activity;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 
 import com.daniel.readingbook.book1.R;
-import com.daniel.readingbook.book1.database.MyDatabase;
-import com.daniel.readingbook.book1.database.table.Book;
 
 public class MainActivity extends ActionBarActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
-
-    private static final int SPLASH_TIMEOUT = 2000;
-    private Handler mHandler;
-
-    private void checkAccountAndLaunchActivity() {
-        MyDatabase myDatabase = new MyDatabase(this);
-        Book book = myDatabase.getBook();
-        Log.i(TAG, book.toString());
-    }
-
-    private void launchSimplePrints() {
-        mHandler = new Handler();
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                checkAccountAndLaunchActivity();
-            }
-        }, SPLASH_TIMEOUT);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        launchSimplePrints();
     }
 }
