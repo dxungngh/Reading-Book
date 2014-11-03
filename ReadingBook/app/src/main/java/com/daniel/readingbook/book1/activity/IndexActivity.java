e0423ace11539643abde08c399e9a455188f5f0b
Daniel Nguyen <dxungngh@gmail.com>
2014-11-03 22:08:01 +0700

    add database helper

--- ReadingBook/app/src/main/java/com/daniel/readingbook/book1/activity/IndexActivity.java
+++ ReadingBook/app/src/main/java/com/daniel/readingbook/book1/activity/IndexActivity.java
@@ -1,19 +1,34 @@
 package com.daniel.readingbook.book1.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ListView;
 
 import com.daniel.readingbook.book1.R;
+import com.daniel.readingbook.book1.database.MyDatabaseHelper;
+
+import java.util.ArrayList;
 
 public class IndexActivity extends ActionBarActivity {
     private static final String TAG = IndexActivity.class.getSimpleName();
+
+    private ListView mChaptersListView;
+    private ArrayList<Chapter> mChapters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+
+        initData();
+        setComponentView();
+    }
+
+    private void initData() {
+        MyDatabaseHelper.getMyDatabase(this).getBook();
+    }
+
+    private void setComponentView() {
+        mChaptersListView = (ListView) findViewById(R.id.index_list_of_chapters);
     }
 }
---
+++ ReadingBook/app/src/main/java/com/daniel/readingbook/book1/database/MyDatabaseHelper.java
@@ -0,0 +1,16 @@
+package com.daniel.readingbook.book1.database;
+
+import android.content.Context;
+
+public class MyDatabaseHelper {
+    private static final String TAG = MyDatabaseHelper.class.getSimpleName();
+
+    private static MyDatabase myDatabase;
+
+    public static MyDatabase getMyDatabase(Context context) {
+        if (myDatabase == null) {
+            myDatabase = new MyDatabase(context);
+        }
+        return myDatabase;
+    }
+}
