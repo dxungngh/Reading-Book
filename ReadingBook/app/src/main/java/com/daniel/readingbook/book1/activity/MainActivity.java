370386f0c41b3761ffc21cb18ed39e4638880dba
Daniel Nguyen <dxungngh@gmail.com>
2014-11-03 21:48:17 +0700

    add listener

--- ReadingBook/app/src/main/java/com/daniel/readingbook/book1/activity/MainActivity.java
+++ ReadingBook/app/src/main/java/com/daniel/readingbook/book1/activity/MainActivity.java
@@ -2,15 +2,61 @@ package com.daniel.readingbook.book1.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.daniel.readingbook.book1.R;
 
 public class MainActivity extends ActionBarActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private Button mReadBookButton, mAddBookButton, mExitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setComponentViews();
+        setListeners();
+    }
+
+    private void setComponentViews() {
+        mReadBookButton = (Button) findViewById(R.id.main_read_book);
+        mAddBookButton = (Button) findViewById(R.id.main_add_book);
+        mExitButton = (Button) findViewById(R.id.main_exit);
+    }
+
+    private void setListeners() {
+        setReadBookListener();
+        setAddBookListener();
+        setExitListener();
+    }
+
+    private void setAddBookListener() {
+        mAddBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void setReadBookListener() {
+        mReadBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void setExitListener() {
+        mExitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
